c4a3753549ac9a8b1149ebb4f8d571b9398e78d6
Ryan O'Leary <roleary305@gmail.com>
2017-11-02 12:09:44 +0700

    updating all scripts

--- RWD/GraphicsProject/allscripts.R
+++ RWD/GraphicsProject/allscripts.R
@@ -1,5 +1,7 @@
  
- #Reads in the Data and sub sets it to only the days required
+ #Reads in the Data formats and sub sets it
+ # All PlotScripts wil check if powerset already exists and only read it in iff neccisary.
+ getandsubset <- function(){
  rawpowerdata <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
  
  dtg <- paste(rawpowerdata$Date,rawpowerdata$Time)
@@ -11,40 +13,51 @@
  powerset$Sub_metering_1 <- as.numeric(as.character(powerset$Sub_metering_1)) 
  powerset$Sub_metering_2 <- as.numeric(as.character(powerset$Sub_metering_2)) 
  powerset$Sub_metering_3 <- as.numeric(as.character(powerset$Sub_metering_3)) 
- 
+ return powerset
+ }
  
  #Creates plot 1
- png(filename = "Plot1.png", width = 480, height = 480, units = "px")
- with(powerset, hist(Global_active_power,  xlab = "Global Active Power (Killowatts)", col = "red", main = "Global Active Power"))
- dev.off()
+if (!exists("powerset"){ powerset <- getandsubset }
+	png(filename = "Plot1.png", width = 480, height = 480, units = "px")
+	with(powerset, hist(Global_active_power,  xlab = "Global Active Power (Killowatts)", col = "red", main = "Global Active Power"))
+ 
+	dev.off()
  
  #Creats plot 2
- png(filename = "Plot2.png", width = 480, height = 480, units = "px")
- with(powerset, plot(DTG, Global_active_power, type = "l", ylab = "Global Active Power (killowatts)", xlab = ""))
- dev.off()
+if (!exists("powerset"){ powerset <- getandsubset }
+	png(filename = "Plot2.png", width = 480, height = 480, units = "px")
+	with(powerset, plot(DTG, Global_active_power, type = "l", ylab = "Global Active Power (killowatts)", xlab = ""))
+	
+	dev.off()
  
  #Creates plot 3
- 
- png(filename = "Plot3.png", width = 480, height = 480, units = "px")
-   dev.off()
+ if (!exists("powerset"){ powerset <- getandsubset }
+	png(filename = "Plot3.png", width = 480, height = 480, units = "px")
+    plot(powerset$DTG, powerset$Sub_metering_1, type = "l", ylab = "Energy Sub Metering", xlab = "")
+		points(powerset$DTG, powerset$Sub_metering_2, type = "l", col = "red")
+		points(powerset$DTG, powerset$Sub_metering_3, type = "l", col = "blue")
+	
+	dev.off()
  
  # Create plot 4
- 
- png(filename = "Plot4.png", width = 480, height = 480, units = "px")
- par(mfrow = c(2,2))
+ if (!exists("powerset"){ powerset <- getandsubset }
+	png(filename = "Plot4.png", width = 480, height = 480, units = "px")
+	par(mfrow = c(2,2))
  
  #Global Active
- plot(powerset$DTG, powerset$Global_active_power, type = "l", ylab = "Global Active Power (killowatts)", xlab = "")
+	plot(powerset$DTG, powerset$Global_active_power, type = "l", ylab = "Global Active Power (killowatts)", xlab = "")
  
  # Voltage
- plot(powerset$DTG, powerset$Voltage, type = "l", ylab = "Voltage", xlab = "")
+	
+	plot(powerset$DTG, powerset$Voltage, type = "l", ylab = "Voltage", xlab = "")
  
  # Sub metering
- plot(powerset$DTG, powerset$Sub_metering_1, type = "l", ylab = "Energy Sub Metering", xlab = "")
- points(powerset$DTG, powerset$Sub_metering_2, type = "l", col = "red")
- points(powerset$DTG, powerset$Sub_metering_3, type = "l", col = "blue")
+	
+	plot(powerset$DTG, powerset$Sub_metering_1, type = "l", ylab = "Energy Sub Metering", xlab = "")
+		points(powerset$DTG, powerset$Sub_metering_2, type = "l", col = "red")
+		points(powerset$DTG, powerset$Sub_metering_3, type = "l", col = "blue")
 
  # Global Reactive
- plot(powerset$DTG, powerset$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
+	plot(powerset$DTG, powerset$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
  
- dev.off()
+	dev.off()
